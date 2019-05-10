ae18fe91d167db0da7f4061cf9b675321d8f00e0
Pavan <pavanteja.bhatta@gmail.com>
2019-05-10 18:42:44 +1000

    EOD Commit 10 May 19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CareersComponent } from './careers/careers.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent,
+    CareersComponent,
+    AboutUsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
